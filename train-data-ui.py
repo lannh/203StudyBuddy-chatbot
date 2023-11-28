bb770a07699f4f754fe5b00b159c16f82ae284ef
lannh <lannh2207@gmail.com>
2023-11-28 15:47:33 -0800

    add password to train-data-ui app

--- train-data-ui.py
+++ train-data-ui.py
@@ -154,32 +154,34 @@ def main():
     print("Exit")
 
 
-# def check_password():
-#     # Returns `True` if the user had the correct password.
+def check_password():
+    # Returns `True` if the user had the correct password.
 
-#     def password_entered():
-#         # Checks whether a password entered by the user is correct.
-#         if hmac.compare_digest(st.session_state["password"], st.secrets["PASSWORD"]):
-#             st.session_state["password_correct"] = True
-#             del st.session_state["password"]  # Don't store the password.
-#         else:
-#             st.session_state["password_correct"] = False
+    def password_entered():
+        # Checks whether a password entered by the user is correct.
+        if hmac.compare_digest(st.session_state["password"], st.secrets["PASSWORD"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
 
-#     # Return True if the password is validated.
-#     if st.session_state.get("password_correct", False):
-#         return True
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
 
-#     # Show input for password.
-#     st.text_input(
-#         "Password", type="password", on_change=password_entered, key="password"
-#     )
-#     if "password_correct" in st.session_state:
-#         st.error("Password incorrect. Please try again.")
-#     return False
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("Password incorrect. Please try again.")
+    return False
 
 
-# if not check_password():
-#     st.stop()  # Do not continue if check_password is not True.
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 if __name__ == '__main__':
-    main()
+    if check_password():
+        # st.stop()  # Do not continue if check_password is not True.
+        main()
